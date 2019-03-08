9717e1a967c3a1080ced5a52fd5c8d0edd1b2419
Zhanhao Wong <shihuihzh@qq.com>
2019-03-08 20:45:13 +0800

    project init

---
+++ desktop/src/com/hzh/chip8emu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hzh.chip8emu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hzh.chip8emu.Chip8emu;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Chip8emu(), config);
+	}
+}
